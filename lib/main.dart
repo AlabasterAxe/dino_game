aff901e4344d4eaa72b627280166a5fa69ad2376
Matthew Keller <matthew.ed.keller@gmail.com>
2020-10-20 14:44:44 -0400

    final version of the code from the blog post

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'cactus.dart';
 import 'cloud.dart';
@@ -10,7 +9,6 @@ import 'constants.dart';
 import 'dino.dart';
 import 'game-object.dart';
 import 'ground.dart';
-import 'ptera.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,12 +19,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Dino',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Dino Home Page'),
     );
   }
 }
@@ -40,65 +38,71 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  AnimationController worldController;
-  Duration lastUpdateCall = Duration();
-  Random rand = Random();
-
-  List<GameObject> obstacles;
-
-  List<Ground> ground;
-
-  List<Cloud> clouds;
-
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   Dino dino = Dino();
   double runDistance = 0;
-  double runSpeed = 30;
-  double best = 0;
+  double runVelocity = 30;
 
-  // this is used to debounce jump clicks that occur right before the user dies
-  bool canReset = false;
-  bool resetStarted = false;
+  AnimationController worldController;
+  Duration lastUpdateCall = Duration();
+
+  List<Cactus> cacti = [Cactus(worldLocation: Offset(200, 0))];
+
+  List<Ground> ground = [
+    Ground(worldLocation: Offset(0, 0)),
+    Ground(worldLocation: Offset(groundSprite.imageWidth / 10, 0))
+  ];
+
+  List<Cloud> clouds = [
+    Cloud(worldLocation: Offset(100, 20)),
+    Cloud(worldLocation: Offset(200, 10)),
+    Cloud(worldLocation: Offset(350, -10)),
+  ];
 
   @override
   void initState() {
     super.initState();
+
     worldController =
         AnimationController(vsync: this, duration: Duration(days: 99));
-
     worldController.addListener(_update);
-
-    _reset();
     worldController.forward();
   }
 
-  void _update() {
-    double elapsedSeconds =
-        ((worldController.lastElapsedDuration.inMilliseconds -
-                lastUpdateCall.inMilliseconds) /
-            1000);
+  void _die() {
+    setState(() {
+      worldController.stop();
+      dino.die();
+    });
+  }
 
+  _update() {
     dino.update(lastUpdateCall, worldController.lastElapsedDuration);
-    runDistance = max(runDistance + runSpeed * elapsedSeconds, 0);
-    runSpeed += RUN_SPEED_ACC_PPSPS * elapsedSeconds;
+
+    double elapsedTimeSeconds =
+        (worldController.lastElapsedDuration - lastUpdateCall).inMilliseconds /
+            1000;
+
+    runDistance += runVelocity * elapsedTimeSeconds;
 
     Size screenSize = MediaQuery.of(context).size;
 
     Rect dinoRect = dino.getRect(screenSize, runDistance);
-    for (GameObject obstacle in obstacles) {
-      Rect obstacleRect = obstacle.getRect(screenSize, runDistance);
-
-      if (dinoRect.deflate(15).overlaps(obstacleRect.deflate(15))) {
+    for (Cactus cactus in cacti) {
+      Rect obstacleRect = cactus.getRect(screenSize, runDistance);
+      if (dinoRect.overlaps(obstacleRect)) {
         _die();
       }
+
       if (obstacleRect.right < 0) {
         setState(() {
-          obstacles.remove(obstacle);
-          obstacles.add(Cactus(
-              worldLocation: Offset(runDistance + rand.nextInt(100) + 50, 0)));
+          cacti.remove(cactus);
+          cacti.add(Cactus(
+              worldLocation:
+                  Offset(runDistance + Random().nextInt(100) + 50, 0)));
         });
       }
-      obstacle.update(lastUpdateCall, worldController.lastElapsedDuration);
     }
 
     for (Ground groundlet in ground) {
@@ -107,8 +111,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
           ground.remove(groundlet);
           ground.add(Ground(
               worldLocation: Offset(
-                  ground.last.worldLocation.dx +
-                      groundSprite.imageWidth / WORLD_TO_PIXEL_RATIO,
+                  ground.last.worldLocation.dx + groundSprite.imageWidth / 10,
                   0)));
         });
       }
@@ -119,9 +122,9 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         setState(() {
           clouds.remove(cloud);
           clouds.add(Cloud(
-              location: Offset(
-                  clouds.last.location.dx + rand.nextInt(100) + 100,
-                  rand.nextInt(100) - 10.0)));
+              worldLocation: Offset(
+                  clouds.last.worldLocation.dx + Random().nextInt(100) + 50,
+                  Random().nextInt(40) - 20.0)));
         });
       }
     }
@@ -129,141 +132,25 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     lastUpdateCall = worldController.lastElapsedDuration;
   }
 
-  void _die() {
-    setState(() {
-      worldController.stop();
-      dino.die();
-      Timer(Duration(milliseconds: 100), () {
-        canReset = true;
-      });
-    });
-  }
-
-  void _reset() {
-    setState(() {
-      if (runDistance > best) {
-        best = runDistance;
-      }
-      runDistance = 0;
-      runSpeed = 30;
-      obstacles = [
-        Cactus(worldLocation: Offset(200, 0)),
-      ];
-
-      clouds = [
-        Cloud(location: Offset(10, 0)),
-        Cloud(location: Offset(200, 0)),
-        Cloud(location: Offset(500, 0)),
-      ];
-      ground = [
-        Ground(worldLocation: Offset(0, 0)),
-        Ground(
-            worldLocation:
-                Offset(groundSprite.imageWidth / WORLD_TO_PIXEL_RATIO, 0))
-      ];
-      dino = Dino();
-      lastUpdateCall = Duration();
-      canReset = false;
-      resetStarted = false;
-    });
-  }
-
-  void _jump() {
-    if ([DinoState.standing, DinoState.running].contains(dino.state)) {
-      if (!worldController.isAnimating) {
-        worldController.forward(from: 0);
-      }
-      setState(() {
-        dino.jump();
-        Timer(Duration(milliseconds: 100), dino.releaseJump);
-      });
-    }
-  }
-
-  String _buttonText() {
-    switch (dino.state) {
-      case DinoState.running:
-      case DinoState.jumping:
-        return "Jump";
-      case DinoState.dead:
-        return "Reset";
-      case DinoState.standing:
-        return "Start";
-      default:
-        return "Jump";
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     List<Widget> children = [];
-    for (Cloud cloud in clouds) {
-      children.add(
-        AnimatedBuilder(
-            animation: worldController,
-            child: cloud.render(),
-            builder: (context, child) {
-              Rect cloudRect = cloud.getRect(screenSize, runDistance);
-              return Positioned(
-                  top: cloudRect.top,
-                  left: cloudRect.left,
-                  width: cloudRect.width,
-                  height: cloudRect.height,
-                  child: child);
-            }),
-      );
-    }
-    for (Ground groundlet in ground) {
-      children.add(
-        AnimatedBuilder(
-            animation: worldController,
-            child: groundlet.render(),
-            builder: (context, child) {
-              Rect groundRect = groundlet.getRect(screenSize, runDistance);
-              return Positioned(
-                  top: groundRect.top,
-                  left: groundRect.left,
-                  width: groundRect.width,
-                  height: groundRect.height,
-                  child: child);
-            }),
-      );
-    }
-    for (GameObject obstacle in obstacles) {
-      children.add(
-        AnimatedBuilder(
-            animation: worldController,
-            builder: (context, child) {
-              Rect obstacleRect = obstacle.getRect(screenSize, runDistance);
-              return Positioned(
-                  top: obstacleRect.top,
-                  left: obstacleRect.left,
-                  width: obstacleRect.width,
-                  height: obstacleRect.height,
-                  child: obstacle.render());
-            }),
-      );
-    }
-    children.add(AnimatedBuilder(
-        animation: worldController,
-        builder: (context, child) {
-          Rect dinoRect = dino.getRect(screenSize, runDistance);
-          return Positioned(
-            left: dinoRect.left,
-            top: dinoRect.top,
-            width: dinoRect.width,
-            height: dinoRect.height,
-            child: dino.render(),
-          );
-        }));
 
-    // if (dino.state == DinoState.dead) {
-    //   children.add(Align(
-    //     alignment: Alignment(0, -.5),
-    //     child: Text("GAME OVER", style: GoogleFonts.vt323(fontSize: 48)),
-    //   ));
-    // }
+    for (GameObject object in [...clouds, ...ground, ...cacti, dino]) {
+      children.add(AnimatedBuilder(
+          animation: worldController,
+          builder: (context, _) {
+            Rect objectRect = object.getRect(screenSize, runDistance);
+            return Positioned(
+              left: objectRect.left,
+              top: objectRect.top,
+              width: objectRect.width,
+              height: objectRect.height,
+              child: object.render(),
+            );
+          }));
+    }
 
     return Scaffold(
       appBar: AppBar(
